5588168ea7e84bb47606c14a1f95c36ce8f85056
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2020-09-19 20:52:38 +0200

    password reset tests finished

--- pages/api/reset/index.test.ts
+++ pages/api/reset/index.test.ts
@@ -4,6 +4,7 @@ import Mail from "nodemailer/lib/mailer";
 import { reset } from "./index";
 
 import getEnv from "../../../lib/test-env";
+import { compare, hash } from "bcrypt";
 
 const LOCAL_ENV = getEnv();
 
@@ -151,4 +152,153 @@ test("set token", async () => {
   expect(transporterArgs.from).toMatch(RegExp(`${LOCAL_ENV.smtp.user}`));
   expect(transporterArgs.to).toMatch(email);
   expect(transporterArgs.subject).toMatch("reset token");
+});
+
+test("set password - returns 401 if token does not match", async () => {
+  const email = "test@google.com";
+  const username = "Tester";
+
+  const { req, res } = createMocks({
+    method: "POST",
+    body: {
+      email,
+      token: "token",
+      password: "password",
+    },
+  });
+
+  req.usersCollection = {
+    async findOne() {
+      return {
+        authenticated: true,
+        email,
+        username,
+        resetToken: {
+          value: "different token",
+          expiration: Date.now() + 1000 * 60 * 60,
+        },
+      };
+    },
+  };
+
+  req.transporter = {
+    async sendMail() {},
+  };
+
+  req.logger = {
+    debug() {},
+    error() {},
+  };
+
+  req.localEnv = LOCAL_ENV;
+
+  const response = await reset(req, res);
+
+  expect(response.statusCode).toEqual(401);
+});
+
+test("set password - returns 401 if token is expired", async () => {
+  const email = "test@google.com";
+  const username = "Tester";
+
+  const { req, res } = createMocks({
+    method: "POST",
+    body: {
+      email,
+      token: "token",
+      password: "password",
+    },
+  });
+
+  req.usersCollection = {
+    async findOne() {
+      return {
+        authenticated: true,
+        email,
+        username,
+        resetToken: {
+          value: "token",
+          expiration: Date.now() - 1000 * 60 * 60,
+        },
+      };
+    },
+  };
+
+  req.transporter = {
+    async sendMail() {},
+  };
+
+  req.logger = {
+    debug() {},
+    error() {},
+  };
+
+  req.localEnv = LOCAL_ENV;
+
+  const response = await reset(req, res);
+
+  expect(response.statusCode).toEqual(401);
+});
+
+test("set password - returns 204 if token passess all checks", async () => {
+  const email = "test@google.com";
+  const username = "Tester";
+  const newPassword = "password";
+
+  const { req, res } = createMocks({
+    method: "POST",
+    body: {
+      email,
+      token: "token",
+      password: newPassword,
+    },
+  });
+
+  let updateArgs = null;
+  req.usersCollection = {
+    async findOne() {
+      return {
+        authenticated: true,
+        email,
+        username,
+        resetToken: {
+          value: "token",
+          expiration: Date.now() + 1000 * 60 * 60,
+        },
+      };
+    },
+    async updateOne(filter, update) {
+      updateArgs = {
+        filter,
+        update,
+      };
+    },
+  };
+
+  let transporterArgs = null;
+  req.transporter = {
+    async sendMail({ from, to, subject }) {
+      transporterArgs = { from, to, subject };
+    },
+  };
+
+  req.logger = {
+    debug() {},
+    error() {},
+  };
+
+  req.localEnv = LOCAL_ENV;
+
+  const response = await reset(req, res);
+
+  expect(response.statusCode).toEqual(204);
+  expect(updateArgs.filter).toEqual({
+    email,
+  });
+  expect(await compare(newPassword, updateArgs.update.password)).toBe(true);
+
+  expect(transporterArgs.from).toMatch(RegExp(`${LOCAL_ENV.mailerName}`));
+  expect(transporterArgs.from).toMatch(RegExp(`${LOCAL_ENV.smtp.user}`));
+  expect(transporterArgs.to).toMatch(email);
+  expect(transporterArgs.subject).toMatch("password reset successful");
 });

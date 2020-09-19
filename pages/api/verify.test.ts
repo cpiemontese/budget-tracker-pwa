4181b9c7e888cff979c1c9737febabd4adebbe22
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2020-09-19 18:14:14 +0200

    add missing test

--- pages/api/verify.test.ts
+++ pages/api/verify.test.ts
@@ -167,3 +167,48 @@ test("returns 401 if token is expired", async () => {
 
   expect(JSON.parse(cookie)).toEqual(null);
 });
+
+test("returns 204 if token is valid", async () => {
+  const { req, res } = createMocks({
+    method: "POST",
+  });
+
+  let actualFilter = null;
+  req.usersCollection = {
+    async findOne(filter) {
+      actualFilter = filter;
+      return {
+        loginToken: "token",
+        loginTokenExpiration: Date.now() + 1000 * 60 * 60, // it expires in an hour ago
+      };
+    },
+  };
+
+  req.transporter = {
+    async sendMail() {},
+  };
+
+  req.logger = {
+    debug() {},
+    error() {},
+  };
+
+  res.setHeader(
+    "Cookie",
+    createCookie(
+      LOCAL_ENV.loginCookie.name,
+      JSON.stringify({
+        email: "test@google.com",
+        loginToken: "token",
+      }),
+      1000 * 60 * 60
+    )
+  );
+
+  req.localEnv = LOCAL_ENV;
+
+  const response = await verify(req, res);
+
+  expect(response.statusCode).toEqual(204);
+  expect(actualFilter).toEqual({ email: "test@google.com" });
+});

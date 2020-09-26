325144e13dd82c2647e02c9cefab5b19dbb5f7af
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2020-09-26 15:04:28 +0200

    add crud handlers to funds api

--- pages/api/funds/[email]/[id].ts
+++ pages/api/funds/[email]/[id].ts
@@ -5,13 +5,17 @@ import logger from "../../../../middleware/logger";
 import envLoader from "../../../../middleware/env-loader";
 import verifier from "../../../../middleware/env-loader";
 
-import { read } from "../../../../lib/crud/funds/read";
+import { createHandler } from "../../../../lib/crud/funds/create";
+import { updateHandler } from "../../../../lib/crud/funds/update";
+import { deleteHandler } from "../../../../lib/crud/funds/delete";
 
 const handler = nextConnect()
   .use(envLoader)
   .use(db)
   .use(logger)
   .use(verifier)
-  .get(read);
+  .post(createHandler)
+  .patch(updateHandler)
+  .delete(deleteHandler);
 
 export default handler;

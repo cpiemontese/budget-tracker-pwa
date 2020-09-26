6f005ed9b4092393d52aa47db731c41291bfdefc
Cristiano Piemontese <cristiano.piemontese@gmail.com>
2020-09-26 11:52:34 +0200

    crud test now uses mongo

--- lib/crud/funds/create.test.ts
+++ lib/crud/funds/create.test.ts
@@ -1,10 +1,17 @@
+import { randomBytes } from "crypto";
+import { MongoClient } from "mongodb";
 import { createMocks } from "node-mocks-http";
 
 import { create } from "./create";
 
 import getEnv from "../../test-env";
+import { User } from "../../../types";
+
+const randomString = () => randomBytes(8).toString("hex");
 
 const LOCAL_ENV = getEnv();
+
+const MONGODB_URI = "mongodb://localhost:27017";
 
 test("returns 400 if properties are missing from query", async () => {
   const { req, res } = createMocks({
@@ -29,6 +36,18 @@ test("returns 201 if fund is created", async () => {
   const name = "fund";
   const amount = 100.0;
 
+  const mongoClient = new MongoClient(MONGODB_URI);
+  await mongoClient.connect();
+  const dbName = randomString();
+  const db = mongoClient.db(dbName);
+  const collectionName = randomString();
+  const collection = db.collection(collectionName);
+
+  collection.insertOne({
+    email,
+    funds: [],
+  });
+
   const { req, res } = createMocks({
     method: "POST",
     query: {
@@ -45,27 +64,25 @@ test("returns 201 if fund is created", async () => {
     error() {},
   };
 
-  let receivedFilter = null;
-  let receivedUpdate = null;
-  req.usersCollection = {
-    async updateOne(filter, update) {
-      receivedFilter = filter;
-      receivedUpdate = update;
-    },
-  };
+  req.usersCollection = collection;
 
   req.localEnv = LOCAL_ENV;
 
   const response = await create(req, res);
 
   expect(response.statusCode).toEqual(201);
-  expect(receivedFilter).toEqual({ email });
-  expect(receivedUpdate).toMatchObject({
-    $push: {
-      funds: {
-        name,
-        amount,
-      },
-    },
+
+  const user: User = await collection.findOne({ email });
+
+  expect(user.funds.length).toBe(1);
+  expect(user.funds[0]).toMatchObject({
+    name,
+    amount,
   });
+  expect(user.funds[0].id).not.toBe(null);
+  expect(user.funds[0].createdAt).not.toBe(null);
+  expect(user.funds[0].updatedAt).toBe(user.funds[0].createdAt);
+
+  await db.dropDatabase();
+  await mongoClient.close();
 });

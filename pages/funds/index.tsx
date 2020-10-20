fca1cb0daa22684e8047357b206579bd4f3f5411
Cristiano Piemontese <cristiano.piemontese@mia-platform.eu>
2020-10-20 19:32:57 +0200

    refactor css

--- pages/funds/index.tsx
+++ pages/funds/index.tsx
@@ -4,16 +4,15 @@ import { FormEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Layout from "../../components/layout";
+import commonStyles from "../../styles/common.module.css";
 import { createFund } from "../../redux/actions";
 import { ReduxState } from "../../redux/types";
 
 const pageName = "Create a new fund";
 
-const classes = {
-  label: "text-black-500 font-bold md:text-right mb-1 md:mb-0 pr-4",
-  input:
-    "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500",
-};
+const formLabel = "text-black-500 font-bold md:text-right mb-1 md:mb-0 pr-4";
+const formInput =
+  "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500";
 
 export default function FundPage() {
   const [name, setName] = useState("");
@@ -48,15 +47,12 @@ export default function FundPage() {
       </Head>
       <form onSubmit={handleSubmit} className="w-full">
         <div className="md:flex md:items-center mb-6">
-          <label
-            className={`md:w-1/3 block ${classes.label}`}
-            htmlFor="name-input"
-          >
+          <label className={`md:w-1/3 block ${formLabel}`} htmlFor="name-input">
             Name
           </label>
           <input
             id="name-input"
-            className={`md:w-2/3 ${classes.input}`}
+            className={`md:w-2/3 ${formInput}`}
             type="text"
             value={name}
             onChange={(event) => setName(event.target.value)}
@@ -64,14 +60,14 @@ export default function FundPage() {
         </div>
         <div className="md:flex md:items-center mb-6">
           <label
-            className={`md:w-1/3 block ${classes.label}`}
+            className={`md:w-1/3 block ${formLabel}`}
             htmlFor="amount-input"
           >
             Amount
           </label>
           <input
             type="number"
-            className={`md:w-2/3 ${classes.input}`}
+            className={`md:w-2/3 ${formInput}`}
             step="0.01"
             value={amount}
             onChange={(event) => setAmount(parseFloat(event.target.value))}
@@ -79,7 +75,7 @@ export default function FundPage() {
         </div>
         <div>
           <input
-            className="w-full p-2 bg-green-500 hover:bg-green-600 active:bg-green-700 text-white font-bold rounded"
+            className={`w-full ${commonStyles.btn} ${commonStyles["btn-green"]}`}
             type="submit"
             value="Create"
           />

063233193eef1dc1d62247f1d7a6532f4725a441
Cristiano Piemontese <cristiano.piemontese@mia-platform.eu>
2020-10-25 12:15:02 +0100

    add fund update

--- pages/funds/[id].tsx
+++ pages/funds/[id].tsx
@@ -1,27 +1,20 @@
-import Head from "next/head";
+import get from "lodash.get";
+
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Layout from "../../components/layout";
-import commonStyles from "../../styles/common.module.css";
+import logger from "../../lib/logger";
+import Fund from "../../components/fund";
+import { ReduxState } from "../../redux/types";
 import {
   updateFund,
   syncFailure,
   syncRequest,
   syncSuccess,
 } from "../../redux/actions";
-import { ReduxState } from "../../redux/types";
-import logger from "../../lib/logger";
-import get from "lodash.get";
 
 const log = logger();
-
-const pageName = "Create a new fund";
-
-const formLabel = "text-black-500 font-bold md:text-right mb-1 md:mb-0 pr-4";
-const formInput =
-  "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500";
 
 export default function FundPage() {
   const router = useRouter();
@@ -38,7 +31,7 @@ export default function FundPage() {
 
   const dispatch = useDispatch();
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  function submitHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     dispatch(updateFund(id, name, amount));
     router.push("/");
@@ -55,46 +48,13 @@ export default function FundPage() {
   }
 
   return (
-    <Layout overrideName={pageName}>
-      <Head>
-        <title>{pageName}</title>
-      </Head>
-      <form onSubmit={handleSubmit} className="w-full">
-        <div className="md:flex md:items-center mb-6">
-          <label className={`md:w-1/3 block ${formLabel}`} htmlFor="name-input">
-            Name
-          </label>
-          <input
-            id="name-input"
-            className={`md:w-2/3 ${formInput}`}
-            type="text"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <label
-            className={`md:w-1/3 block ${formLabel}`}
-            htmlFor="amount-input"
-          >
-            Amount
-          </label>
-          <input
-            type="number"
-            className={`md:w-2/3 ${formInput}`}
-            step="0.01"
-            value={amount}
-            onChange={(event) => setAmount(parseFloat(event.target.value))}
-          />
-        </div>
-        <div>
-          <input
-            className={`w-full ${commonStyles.btn} ${commonStyles["btn-blue"]}`}
-            type="submit"
-            value="Update"
-          />
-        </div>
-      </form>
-    </Layout>
+    <Fund
+      pageName={"Update fund"}
+      name={name}
+      amount={amount}
+      setName={setName}
+      setAmount={setAmount}
+      submitHandler={submitHandler}
+    />
   );
 }
--- pages/funds/index.tsx
+++ pages/funds/index.tsx
@@ -1,26 +1,18 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Layout from "../../components/layout";
-import commonStyles from "../../styles/common.module.css";
+import logger from "../../lib/logger";
+import { ReduxState } from "../../redux/types";
 import {
   createFund,
   syncFailure,
   syncRequest,
   syncSuccess,
 } from "../../redux/actions";
-import { ReduxState } from "../../redux/types";
-import logger from "../../lib/logger";
+import Fund from "../../components/fund";
 
 const log = logger();
-
-const pageName = "Create a new fund";
-
-const formLabel = "text-black-500 font-bold md:text-right mb-1 md:mb-0 pr-4";
-const formInput =
-  "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500";
 
 export default function FundPage() {
   const [name, setName] = useState("");
@@ -34,7 +26,7 @@ export default function FundPage() {
 
   const dispatch = useDispatch();
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  function submitHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     dispatch(createFund(name, amount));
     router.push("/");
@@ -51,46 +43,13 @@ export default function FundPage() {
   }
 
   return (
-    <Layout overrideName={pageName}>
-      <Head>
-        <title>{pageName}</title>
-      </Head>
-      <form onSubmit={handleSubmit} className="w-full">
-        <div className="md:flex md:items-center mb-6">
-          <label className={`md:w-1/3 block ${formLabel}`} htmlFor="name-input">
-            Name
-          </label>
-          <input
-            id="name-input"
-            className={`md:w-2/3 ${formInput}`}
-            type="text"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <label
-            className={`md:w-1/3 block ${formLabel}`}
-            htmlFor="amount-input"
-          >
-            Amount
-          </label>
-          <input
-            type="number"
-            className={`md:w-2/3 ${formInput}`}
-            step="0.01"
-            value={amount}
-            onChange={(event) => setAmount(parseFloat(event.target.value))}
-          />
-        </div>
-        <div>
-          <input
-            className={`w-full ${commonStyles.btn} ${commonStyles["btn-green"]}`}
-            type="submit"
-            value="Create"
-          />
-        </div>
-      </form>
-    </Layout>
+    <Fund
+      pageName={"Create a new fund"}
+      name={name}
+      amount={amount}
+      setName={setName}
+      setAmount={setAmount}
+      submitHandler={submitHandler}
+    />
   );
 }

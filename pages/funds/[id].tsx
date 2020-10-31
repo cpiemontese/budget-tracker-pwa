f595b15cfcc01ceb01ed38ec7180594554f3c93d
Cristiano Piemontese <cristiano.piemontese@mia-platform.eu>
2020-10-31 13:04:44 +0100

    add form common styles

--- pages/funds/[id].tsx
+++ pages/funds/[id].tsx
@@ -10,10 +10,6 @@ import commonStyles from "../../styles/common.module.css";
 import EntityUpdateForm from "../../components/entity-update-form";
 import { ReduxState } from "../../redux/types";
 
-const formLabel = "text-black-500 font-bold md:text-right mb-1 md:mb-0 pr-4";
-const formInput =
-  "bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500";
-
 export default function UpdateFund() {
   const router = useRouter();
   const id = router.query.id as string;
@@ -40,14 +36,14 @@ export default function UpdateFund() {
           <form onSubmit={submitHandler} className="w-full">
             <div className="md:flex md:items-center mb-6">
               <label
-                className={`md:w-1/3 block ${formLabel}`}
+                className={`md:w-1/3 block md:text-right md:mb-0 ${commonStyles["form-label"]}`}
                 htmlFor="name-input"
               >
                 Name
               </label>
               <input
                 id="name-input"
-                className={`md:w-2/3 ${formInput}`}
+                className={`md:w-2/3 ${commonStyles["form-input"]} ${commonStyles["form-input-blue"]}`}
                 type="text"
                 value={name}
                 onChange={(event) => setName(event.target.value)}
@@ -55,14 +51,14 @@ export default function UpdateFund() {
             </div>
             <div className="md:flex md:items-center mb-6">
               <label
-                className={`md:w-1/3 block ${formLabel}`}
+                className={`md:w-1/3 block md:text-right md:mb-0 ${commonStyles["form-label"]}`}
                 htmlFor="amount-input"
               >
                 Amount
               </label>
               <input
                 id="amount-input"
-                className={`md:w-2/3 ${formInput}`}
+                className={`md:w-2/3 ${commonStyles["form-input"]} ${commonStyles["form-input-blue"]}`}
                 type="number"
                 step="0.01"
                 value={amount}

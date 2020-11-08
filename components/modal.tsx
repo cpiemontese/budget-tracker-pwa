340dc178efb59e10261732aedf29a8196b77a541
Cristiano Piemontese <cristiano.piemontese@mia-platform.eu>
2020-11-08 19:59:39 +0100

    quick modal fix

--- components/modal.tsx
+++ components/modal.tsx
@@ -20,7 +20,10 @@ export default function Modal({
           className="fixed inset-0 bg-black bg-opacity-25"
           onClick={() => setVisible(false)}
         ></div>
-        <div className={`${commonStyles.modal} ${commonStyles.smooth}`}>
+        <div
+          className={`${commonStyles.modal} ${commonStyles.smooth}`}
+          style={{ top: "30%" }}
+        >
           <div className="mb-4 font-semibold text-xl border-b-2">{title}</div>
           {children}
           {closeBtn && (
